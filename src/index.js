df73a9d22ceb29cdd7bdd06241f1e39eb646a4ea
Lusorich <dreamonautre@gmail.com>
2019-12-15 23:26:19 +0500

    hw_1 done

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ process.argv.forEach((arg, index, array) => {
     dirArray.push(myDir);
   } else if (arg === "--target") {
     newDir = array[index + 1];
-    fs.mkdir(newDir, err => {
-      console.log(err);
-    });
+    if (!isDirExist(newDir)) {
+      fs.mkdirSync(newDir);
+    }
   } else if (arg === "--delete") {
     needDelete = true;
   }
@@ -24,49 +24,40 @@ process.argv.forEach((arg, index, array) => {
 
 const fileSort = (myDir, newDir, dirArray) => {
   //читаем переданную директорую
-  fs.readdir(myDir, (err, files) => {
-    if (err) {
-      console.log("ERROR, MY FRIEND");
+  const syncDir = fs.readdirSync(myDir);
+  //проходимся по всем элементам в папке
+  syncDir.forEach(file => {
+    let stat = fs.statSync(path.resolve(myDir, file));
+    //если элемент файл, создаем директорию по первой букве
+    if (stat.isFile()) {
+      let newPath = path.join(newDir, path.basename(file)[0]);
+      if (!isDirExist(newPath)) {
+        fs.mkdirSync(newPath);
+      }
+      //копируем файл в новую директорию
+      fs.copyFileSync(path.join(myDir, file), path.join(newPath, file));
+      //если есть флаг delete, после копирования сразу удаляем файл
+      if (needDelete) {
+        fs.unlinkSync(path.join(myDir, file));
+      }
     }
-    //проходимся по всем элементам в папке
-    files.forEach(file => {
-      let stat = fs.statSync(path.resolve(myDir, file));
-      //если элемент файл, создаем директорию по первой букве
-      if (stat.isFile() === true) {
-        let newPath = path.join(newDir, path.basename(file)[0]);
-        fs.mkdir(newPath, err => {
-          console.log(err);
-          //копируем файл в новую директорию
-          fs.copyFile(path.join(myDir, file), path.join(newPath, file), err => {
-            //если есть флаг delete, после копирования сразу удаляем файл
-            if (needDelete) {
-              fs.unlink(path.join(myDir, file), err => {
-                console.log(err);
-              });
-            }
-          });
-        });
+    if (stat.isDirectory()) {
+      //если стоит флаг delete, то формируем массив со всеми папками, проверяем чтобы папки не дублировались
+      //на случай если в 1й папке несколько файлов
+      if (needDelete && !dirArray.includes(path.join(myDir, file))) {
+        dirArray.push(path.join(myDir, file));
       }
-      if (stat.isDirectory() === true) {
-        //если стоит флаг delete, то формируем массив со всеми папками, проверяем чтобы папки не дублировались
-        //на случай если в 1й папке несколько файлов
-        if (needDelete && dirArray.includes(path.join(myDir, file)) === false) {
-          dirArray.push(path.join(myDir, file));
-        }
-        //если элемент это директория, вызываем заново функцию с новым путем
-        fileSort(path.join(myDir, file), newDir, dirArray);
-      }
-    });
+      //если элемент это директория, вызываем заново функцию с новым путем
+      fileSort(path.join(myDir, file), newDir, dirArray);
+    }
   });
   if (needDelete) {
-    setTimeout(() => delDir(dirArray), 5000);
+    fs.rmdirSync(myDir);
   }
 };
-//удаление всех каталогов, которые занесены в массив
-function delDir(dirArray) {
-  for (let i = dirArray.length - 1; i >= 0; i--) {
-    fs.rmdirSync(dirArray[i]);
-  }
+//проверка на существование
+function isDirExist(path) {
+  return fs.existsSync(path);
 }
 
 fileSort(myDir, newDir, dirArray);
