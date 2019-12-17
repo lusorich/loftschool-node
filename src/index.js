c279a5c5e107e7246d806e781300bbe4d129bcb8
Lusorich <dreamonautre@gmail.com>
2019-12-18 00:17:18 +0500

    hw 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 
 let myDir,
   newDir,
-  needDelete = false;
+  needDelete = false,
+  interval,
+  stopInterval;
 
-//Считываем через аргументы начальную и конечную папки, смотрим надо ли удалять начальную
-//создаем конечную папку
 process.argv.forEach((arg, index, array) => {
   if (arg === "--default") {
     myDir = array[index + 1];
@@ -17,42 +18,19 @@ process.argv.forEach((arg, index, array) => {
     }
   } else if (arg === "--delete") {
     needDelete = true;
+  } else if (arg === "--setInterval") {
+    interval = array[index + 1];
+  } else if (arg === "--setIntervalStop") {
+    stopInterval = array[index + 1];
   }
 });
 
-const fileSort = (myDir, newDir) => {
-  //читаем переданную директорую
-
-  const syncDir = fs.readdirSync(myDir);
-  //проходимся по всем элементам в папке
-  syncDir.forEach(file => {
-    let stat = fs.statSync(path.resolve(myDir, file));
-    //если элемент файл, создаем директорию по первой букве
-    if (stat.isFile()) {
-      let newPath = path.join(newDir, path.basename(file)[0]);
-      if (!isDirExist(newPath)) {
-        fs.mkdirSync(newPath);
-      }
-      //копируем файл в новую директорию
-      fs.copyFileSync(path.join(myDir, file), path.join(newPath, file));
-      //если есть флаг delete, после копирования сразу удаляем файл
-      if (needDelete) {
-        fs.unlinkSync(path.join(myDir, file));
-      }
-    }
-    if (stat.isDirectory()) {
-      //если элемент это директория, вызываем заново функцию с новым путем
-      fileSort(path.join(myDir, file), newDir);
-    }
-  });
-  if (needDelete) {
-    console.log(myDir);
-    fs.rmdirSync(myDir);
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (url === "/start" && method === "GET") {
+    console.log(interval);
+    return res.end();
   }
-};
-//проверка на существование
-function isDirExist(path) {
-  return fs.existsSync(path);
-}
+});
 
-fileSort(myDir, newDir);
+server.listen(3030);
