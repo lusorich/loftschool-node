8cea5d81002872484510493ac20a8896300cc3da
Lusorich <dreamonautre@gmail.com>
2020-01-04 14:57:56 +0500

    without setTimeout

--- src/http.js
+++ src/http.js
@@ -3,7 +3,7 @@ const http = require("http");
 let setIntervalStart, setIntervalStop;
 
 process.argv.forEach((arg, index, array) => {
-  if (arg === "--setInterval") {
+  if (arg === "--setIntervalStart") {
     setIntervalStart = array[index + 1];
   } else if (arg === "--setIntervalStop") {
     setIntervalStop = array[index + 1];
@@ -20,14 +20,15 @@ const server = http.createServer((req, res) => {
 const promise = res => {
   return new Promise((resolve, reject) => {
     console.log(`Начало ${new Date().toUTCString()}`);
+    let startDate = (new Date().getTime() + Number(setIntervalStart));
+    let endDate = ((startDate + Number(setIntervalStop))/1000).toFixed(1);
     let int = setInterval(() => {
       console.log(`В интервале ${new Date().toUTCString()}`);
+      if ( ((new Date().getTime())/1000).toFixed(1) === endDate) {
+        clearInterval(int);
+        resolve(res.end(`Конец ${new Date().toUTCString()}`));
+      }
     }, setIntervalStart);
-    setTimeout(() => {
-      clearInterval(int);
-      console.log(`Время отключения Конец ${new Date().toUTCString()}`);
-      resolve(res.end(`Конец ${new Date().toUTCString()}`));
-    }, setIntervalStop);
   });
 };
 
