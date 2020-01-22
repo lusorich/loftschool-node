fac3a3dc4721712ef9d5fee4f7f8c55692e4233a
Lusorich <dreamonautre@gmail.com>
2020-01-23 01:33:52 +0500

    done

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('./models/data.json');
+const adapter = new FileSync('data.json');
 const db = low(adapter);
 
 module.exports = db;
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('data.json');
-const db = low(adapter);
+const db = require('../db');
 
 db.defaults({ products: [], skills: {} });
 
